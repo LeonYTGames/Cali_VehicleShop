a5df000337bdd42d3cf0fa5b60c578b190682a0a
Leon <leondietrich20084@gmail.com>
2025-03-19 15:08:33 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,26 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name "Cali_VehicleShop"
+description "California Life Roleplay V2 Vehicleshop"
+author "LeonYTGames"
+version "1.0"
+
+shared_scripts {
+	'shared/*.lua'
+}
+
+client_scripts {
+	'client/*.lua'
+}
+
+server_scripts {
+	'server/*.lua'
+}
+
+ui_page "html/index.html"
+
+files "html/css/*.css"
+files "html/js/*.js"
+files "html/image/*.png"
+files "html/image/*.jpg"
